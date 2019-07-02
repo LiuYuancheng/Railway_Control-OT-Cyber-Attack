84e48fdf1fd996fc1cdf025dc42e66fd7741d0e7
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-07-02 18:05:09 +0800

    Agent module to connect to PLC

---
+++ src/railwayAgentPLC.py
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------------------
+# Name:        railwayAgent.py
+#
+# Purpose:     This module is the agent module to communicate to PLC 
+#              
+# Author:      Yuancheng Liu
+#
+# Created:     2019/07/02
+# Copyright:   YC
+# License:     YC
+#-----------------------------------------------------------------------------
