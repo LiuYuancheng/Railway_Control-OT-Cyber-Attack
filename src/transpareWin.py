993568be57ea00d2b9b73f8333acb6ead3be7a1c
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-08-01 13:26:28 +0800

    Fixed bug

--- src/transpareWin.py
+++ src/transpareWin.py
@@ -1,23 +1,30 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        transparentWin.py
+#
+# Purpose:     This module is used to create a transparent window. we did some 
+#              small changes from the module: https://wiki.wxpython.org/Transparent%20Frames
+#              
+# Author:      Yuancheng Liu
+#
+# Created:     2019/07/31
+#-----------------------------------------------------------------------------
 import wx
-import railwayPanel as rwp
 
-# https://wiki.wxpython.org/Transparent%20Frames
-class AppFrame( wx.Frame )  :
-
-    def __init__( self )  :
-
+class transparentWin(wx.Frame):
+    def __init__(self):
         wx.Frame.__init__( self, None, title="Am I transparent?",
                            style=wx.CAPTION | wx.STAY_ON_TOP )
         self.SetClientSize( (300, 300) )
-        self.SetBackgroundColour(wx.Colour('RED'))
+        self.SetBackgroundColour(wx.Colour('WHITE'))
         self.alphaValue = 255
         self.alphaIncrement = -4
 
-        pnl = rwp.CameraView(self, 0)
-        pnl.setPlay()
-        #wx.Panel( self )
-        #self.stTxt = wx.StaticText( pnl, -1, str( self.alphaValue ), (25, 25) )
-        #self.stTxt.SetFont( wx.Font( 18, wx.SWISS, wx.NORMAL, wx.NORMAL ) )
+        #pnl = rwp.CameraView(self, 0)
+        # pnl.setPlay()
+        baPanel = wx.Panel(self)
+        self.stTxt = wx.StaticText( baPanel, -1, str( self.alphaValue ), (25, 25) )
+        self.stTxt.SetFont( wx.Font( 18, wx.SWISS, wx.NORMAL, wx.NORMAL ) )
 
         self.changeAlpha_timer = wx.Timer( self )
         self.changeAlpha_timer.Start( 50 )       # 20 changes per second
@@ -25,10 +32,8 @@ class AppFrame( wx.Frame )  :
 
         self.Bind( wx.EVT_CLOSE, self.OnCloseWindow )
 
-    #end AppFrame class
-
+    #end transparentWin class
     #--------------------------------------------------------
-
     def ChangeAlpha( self, evt )  :
         """ The term "alpha" means variable transparency
               as opposed to a "mask" which is binary transparency.
@@ -113,13 +118,13 @@ class AppFrame( wx.Frame )  :
         #end if
     #end MakeTransparent def
 
-#end AppFrame class
+#end transparentWin class
 
 #=======================================================
 
 if __name__ == '__main__' :
 
     app = wx.App( False )
-    frm = AppFrame()
+    frm = transparentWin()
     frm.Show()
     app.MainLoop()
