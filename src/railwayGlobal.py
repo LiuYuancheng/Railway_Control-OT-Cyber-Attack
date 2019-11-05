bc896796a710ef7682d32a9f13d11a37d7fcda62
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-11-05 18:02:36 +0800

    replace the normal path generate part by Use os.path.join().

--- src/railwayGlobal.py
+++ src/railwayGlobal.py
@@ -49,65 +49,66 @@ PLC_CFG = {
 
 #------<IMAGES PATH>-------------------------------------------------------------
 # Title icon path:
-TTICO_PATH = "".join([dirpath, "\\img\\title.ico"])
+IMG_FD = 'img'
+TTICO_PATH = os.path.join(dirpath, IMG_FD, "title.ico")
 
 # Panel background path:
-BGPNG_PATH = "".join([dirpath, "\\img\\Trainbg1.png"])      # map panel.
-RAJPG_PATH = "".join([dirpath, "\\img\\ransomwareAtt.jpg"]) # ransomeare attack panel.
-TAGIF_PATH = "".join([dirpath, "\\img\\trojanAtt.gif"])     # tronjan attack panel.
+BGPNG_PATH = os.path.join(dirpath, IMG_FD, "Trainbg1.png")      # map panel.
+RAJPG_PATH = os.path.join(dirpath, IMG_FD, "ransomwareAtt.jpg") # ransomeare attack panel.
+TAGIF_PATH = os.path.join(dirpath, IMG_FD, "trojanAtt.gif")     # tronjan attack panel.
 
 # Pedestrian cross signal at the gate: 
-WKJPG_PATH = "".join([dirpath, "\\img\\walk.jpg"])          # Pedestrian pass.
-HTPNG_PATH = "".join([dirpath, "\\img\\hitPPl.png"])        # Pedestrian accident.
-PPPNG_PATH = "".join([dirpath, "\\img\\passS18.png"])       # signal for people pass 
-PSPNG_PATH = "".join([dirpath, "\\img\\stopS18.png"])       # signal for people stop 
-CPPNG_PATH = "".join([dirpath, "\\img\\gateCarP18.png"])    # signal for cars pass    
-CSPNG_PATH = "".join([dirpath, "\\img\\gateCarS18.png"])    # signal for cars stop.
+WKJPG_PATH = os.path.join(dirpath, IMG_FD, "walk.jpg")          # Pedestrian pass.
+HTPNG_PATH = os.path.join(dirpath, IMG_FD, "hitPPl.png")        # Pedestrian accident.
+PPPNG_PATH = os.path.join(dirpath, IMG_FD, "passS18.png")       # signal for people pass 
+PSPNG_PATH = os.path.join(dirpath, IMG_FD, "stopS18.png")       # signal for people stop 
+CPPNG_PATH = os.path.join(dirpath, IMG_FD, "gateCarP18.png")    # signal for cars pass    
+CSPNG_PATH = os.path.join(dirpath, IMG_FD, "gateCarS18.png")    # signal for cars stop.
 
 # Train setting panel path:
-LDSET_PATH = "".join([dirpath, "\\img\\loadSet32.png"])     # load setting.    
-EMGST_PATH = "".join([dirpath, "\\img\\emgStop32.png"])     # emergency stop.
-RECOV_PATH = "".join([dirpath, "\\img\\reset32.png"])       # train station recover.
+LDSET_PATH = os.path.join(dirpath, IMG_FD, "loadSet32.png")     # load setting.    
+EMGST_PATH = os.path.join(dirpath, IMG_FD, "emgStop32.png")     # emergency stop.
+RECOV_PATH = os.path.join(dirpath, IMG_FD, "reset32.png")       # train station recover.
 
 # Railway fork signal:
-FSPNG_PATH = "".join([dirpath, "\\img\\forkOnS18.png"])     # fork go straight signal. 
-FAPNG_PATH = "".join([dirpath, "\\img\\forkAoffS18.png"])   # railway A fork on signal.
-FBPNG_PATH = "".join([dirpath, "\\img\\forkBoffS18.png"])   # railway B fork on signal.
+FSPNG_PATH = os.path.join(dirpath, IMG_FD, "forkOnS18.png")     # fork go straight signal. 
+FAPNG_PATH = os.path.join(dirpath, IMG_FD, "forkAoffS18.png")   # railway A fork on signal.
+FBPNG_PATH = os.path.join(dirpath, IMG_FD, "forkBoffS18.png")   # railway B fork on signal.
 
 # Station signal light: 
-SOPNG_PATH = "".join([dirpath, "\\img\\stationOn18.png"])   # station signal on.
-SFPNG_PATH = "".join([dirpath, "\\img\\stationOff18.png"])  # station signal off.
+SOPNG_PATH = os.path.join(dirpath, IMG_FD, "stationOn18.png")   # station signal on.
+SFPNG_PATH = os.path.join(dirpath, IMG_FD, "stationOff18.png")  # station signal off.
 
 # Power plant light:
-POPNG_PATH = "".join([dirpath, "\\img\\powerPlantOn.png"])  # powerplant light on.
-PFPNG_PATH = "".join([dirpath, "\\img\\powerPlantOff.png"]) # powerplant light off.
+POPNG_PATH = os.path.join(dirpath, IMG_FD, "powerPlantOn.png")  # powerplant light on.
+PFPNG_PATH = os.path.join(dirpath, IMG_FD, "powerPlantOff.png") # powerplant light off.
 
 # Industrial Area. 
-INOPNG_PATH = "".join([dirpath, "\\img\\industrOn.png"])    # industrial area light on.
-INFPNG_PATH = "".join([dirpath, "\\img\\industrOff.png"])   # industrial area light off.
+INOPNG_PATH = os.path.join(dirpath, IMG_FD, "industrOn.png")    # industrial area light on.
+INFPNG_PATH = os.path.join(dirpath, IMG_FD, "industrOff.png")   # industrial area light off.
 
 # City area light
-CTOPNG_PATH = "".join([dirpath, "\\img\\cityOn.png"])       # city area light on.
-CTFPNG_PATH = "".join([dirpath, "\\img\\cityOff.png"])      # city area light off.
+CTOPNG_PATH = os.path.join(dirpath, IMG_FD, "cityOn.png")       # city area light on.
+CTFPNG_PATH = os.path.join(dirpath, IMG_FD, "cityOff.png")      # city area light off.
 
 # Residential area light
-RDOPNG_PATH = "".join([dirpath, "\\img\\residentialOn.png"]) # Residential area light on.     
-RDFPNG_PATH = "".join([dirpath, "\\img\\residentialOff.png"])# Residential area light off.   
+RDOPNG_PATH = os.path.join(dirpath, IMG_FD, "residentialOn.png") # Residential area light on.     
+RDFPNG_PATH = os.path.join(dirpath, IMG_FD, "residentialOff.png")# Residential area light off.   
 
 # AirPort area light
-APOPNG_PATH = "".join([dirpath, "\\img\\AirportOn.png"])    # AirPort area light on.     
-APFPNG_PATH = "".join([dirpath, "\\img\\AirportOff.png"])   # AirPort area light off.     
+APOPNG_PATH = os.path.join(dirpath, IMG_FD, "AirportOn.png")    # AirPort area light on.     
+APFPNG_PATH = os.path.join(dirpath, IMG_FD, "AirportOff.png")   # AirPort area light off.     
 
 # Station signal light: 
-STONPNG_PATH = "".join([dirpath, "\\img\\stationOn.png"])   # station light on.
-STOFPNG_PATH = "".join([dirpath, "\\img\\stationOff.png"])  # station light off.
+STONPNG_PATH = os.path.join(dirpath, IMG_FD, "stationOn.png")   # station light on.
+STOFPNG_PATH = os.path.join(dirpath, IMG_FD, "stationOff.png")  # station light off.
 
 # Camera signal and the camera panel view:
-CAMPNG_PATH = "".join([dirpath, "\\img\\cameraUp.png"])   # station light on.
-TPSGIP_PATH = "".join([dirpath, "\\img\\trailPass.gif"])   # station light on.
+CAMPNG_PATH = os.path.join(dirpath, IMG_FD, "cameraUp.png")   # station light on.
+TPSGIP_PATH = os.path.join(dirpath, IMG_FD, "trailPass.gif")   # station light on.
 
 # Train clash image:
-CLPNG_PATH = "".join([dirpath, "\\img\\clash.png"]) 
+CLPNG_PATH = os.path.join(dirpath, IMG_FD, "clash.png") 
 
 #-------<GLOBAL PARAMTERS>-----------------------------------------------------
 # Set the global reference here.
