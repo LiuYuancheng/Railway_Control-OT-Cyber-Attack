1cb03b5a56d91b442f1d869c331ca292815d27d4
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2020-01-02 17:43:25 +0800

    Added the UDP server to communicate to the attack server.

--- attack/remoteAtk/attackWeb/attackHost.py
+++ attack/remoteAtk/attackWeb/attackHost.py
@@ -1,5 +1,13 @@
+import socket
 from flask import Flask, redirect, url_for, request, render_template
 
+SEV_IP = ('127.0.0.1', 5005)
+BUFFER_SZ = 1024
+
+# Init the UDP send server
+crtClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Init the flask web server program.
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,18 +19,28 @@ def login():
     if request.method == 'POST':
         if request.form['submit_button'] == 'startAtt1':
             #return render_template('login.html')
+            msg = 'A;1'
+            crtClient.sendto(msg.encode('utf-8'), SEV_IP)
             return redirect(url_for('index'))
         elif request.form['submit_button'] == 'startAtt2':
             #return render_template('login.html')
+            msg = 'A;2'
+            crtClient.sendto(msg.encode('utf-8'), SEV_IP)
             return redirect(url_for('index'))
         elif request.form['submit_button'] == 'stopAtt1':
             #return render_template('login.html')
+            msg = 'A;0'
+            crtClient.sendto(msg.encode('utf-8'), SEV_IP)
             return redirect(url_for('index'))
         elif request.form['submit_button'] == 'stopAtt2':
             #return render_template('login.html')
+            msg = 'A;0'
+            crtClient.sendto(msg.encode('utf-8'), SEV_IP)
             return redirect(url_for('index'))
     elif request.method == 'GET':
         return redirect('/index') 
 
+
 if __name__ == '__main__':
-   app.run(host= "0.0.0.0", debug=True, threaded=True)
+    app.run(host= "0.0.0.0", debug=False, threaded=True)
+   
